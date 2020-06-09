aed6fea736f03aa8903b88f3aefabc6a8b9c6ae5
Diego Mellado <diegome88@gmail.com>
2020-06-09 14:37:58 -0400

    Al menos puedo abrir las señales

--- 00_exploracion.py
+++ 00_exploracion.py
@@ -10,19 +10,16 @@ Created on Thu May 28 13:50:24 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
+from sklearn.manifold import TSNE
 
-
-data = h5py.File('data/ITPCdata15Ch10Rep.mat')
+data = h5py.File('data/ITPCdata15Ch10Rep.mat', mode="r")
 mat = np.array(data['imagenes']) #10860,717,21
 tarea = np.array(data['tarea'], dtype=np.int).squeeze()
 sujeto = np.array(data['sujeto'], dtype=np.int).squeeze()
 
+tsne = TSNE(n_components=2, init="pca")
+x0 = mat.reshape(len(mat), -1)
+xt = tsne.fit_transform(x0)
 
-# Prueba con sujeto 1 y tarea 1
-events = np.logical_and(sujeto == 1, tarea == 1)
-mat_s1 = mat[events]
 
-mean_sigs = np.mean(mat_s1, axis=0)
-plt.plot(mean_sigs)
-
-#
+plt.scatter(xt[:,0], xt[:,1], c=sujeto)
